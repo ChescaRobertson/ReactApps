05c0e3c24d150ff9b4d3d2e672412ef83f17e501
ChescaRobertson <francescamarierobertson@hotmail.co.uk>
2022-05-10 15:25:54 +0100

    Spring Bean added

---
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.example.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    private String message;
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    // Getter
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.example.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
@@ -13,5 +11,11 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    // GetMapping to hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
