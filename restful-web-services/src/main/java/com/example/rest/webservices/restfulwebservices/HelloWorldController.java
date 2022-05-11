9cd07a19f591837d5e945724d84299e7b507ffa9
ChescaRobertson <francescamarierobertson@hotmail.co.uk>
2022-05-11 11:43:38 +0100

    Changes to restful webservice

--- restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,10 +1,13 @@
 package com.example.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 
     //method return "Hello World"
@@ -17,5 +20,10 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path="/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
--- springboot-basics/src/main/java/com/springboot/basics/BookController.java
+++ springboot-basics/src/main/java/com/springboot/basics/BookController.java
@@ -1,2 +1,16 @@
-package com.springboot.basics;public class BookController {
+package com.springboot.basics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(new Book(1l, "Wuthering Heights", "Jane Austen"));
+    }
+
 }
